f36eadbcf14aacb1d906d44e8c4e8b7b1d7e1053
Paulo Romano <pauloromanocarvalho@gmail.com>
2015-12-30 01:04:32 -0200

    Subscription feature

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -1,41 +1,39 @@
 from django.test import TestCase
+from eventex.subscriptions.forms import SubscriptionForm
 
 class SubscribeTest(TestCase):
     def setUp(self):
         self.response = self.client.get('/inscricao/')
 
     def test_get(self):
-        """ Get /incricao/ must return status code 200 """
+        """Get /incricao/ must return status code 200"""
         self.assertEqual(200, self.response.status_code)
 
     def test_template(self):
-        """ Must use subscription_form.html template """
+        """Must use subscription_form.html template"""
         self.assertTemplateUsed(self.response, 'subscription_form.html')
 
-    def test_form_action(self):
-        """ template must have a form with action post """
-        self.assertContains(self.response, 'form action="." method="post"')
-
-    def test_textfield_name(self):
-        """ template must have name text field """
-        self.assertContains(self.response, 'type="text" name="name"')
-
-    def test_textfield_cpf(self):
-        """ template must have cpf text field """
-        self.assertContains(self.response, 'type="text" name="cpf"')
-
-    def test_textfield_email(self):
-        """ template must have email text field """
-        self.assertContains(self.response, 'type="text" name="email"')
-
-    def test_textfield_phone(self):
-        """ template must have phone text field """
-        self.assertContains(self.response, 'type="text" name="phone"')
-
-    def test_submitfield_enviar(self):
-        """ template must have a Enviar button """
-        self.assertContains(self.response, 'type="submit" value="Enviar"')
+    def test_html(self):
+        """Html must contain input tags"""
+        self.assertContains(self.response, '<form')
+        self.assertContains(self.response, '<input', 6)
+        self.assertContains(self.response, 'type="text"', 3)
+        self.assertContains(self.response, 'type="email"')
+        self.assertContains(self.response, 'type="submit"')
 
     def test_csrf(self):
-        """ Html must have csrfmiddlewaretoken """
+        """Html must have csrfmiddlewaretoken"""
         self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_has_form(self):
+        """Context must have a form"""
+        form = self.response.context['form']
+        self.assertIsInstance(form, SubscriptionForm)
+
+    def test_form_has_fields(self):
+        """Form must has fields"""
+        form = self.response.context['form']
+        self.assertSequenceEqual(['name', 'cpf', 'email', 'phone'], list(form.fields))
+
+
+
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from eventex.subscriptions.forms import SubscriptionForm
 
 
 def subscription(request):
-    return render(request, 'subscription_form.html')
+    context = {'form': SubscriptionForm()}
+    return render(request, 'subscription_form.html', context)
