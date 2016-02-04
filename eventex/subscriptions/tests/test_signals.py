eab6ceba447466f040a683ea45ee63e91469916e
Paulo Romano <pauloromanocarvalho@gmail.com>
2016-02-04 21:49:25 -0200

    Subscription created signal must be called with some name in subscriptions:new

--- eventex/subscriptions/tests/test_signals.py
+++ eventex/subscriptions/tests/test_signals.py
@@ -1,6 +1,8 @@
+from django.shortcuts import resolve_url
 from django.test import TestCase
 from django.core.signals import Signal
 from eventex.subscriptions.signals import subscription_created
+from unittest.mock import Mock
 
 
 class SignalsTest(TestCase):
@@ -11,3 +13,15 @@ class SignalsTest(TestCase):
     def test_subscription_created_signal_param(self):
         """Subscription created signal must have name param"""
         self.assertIn('name', subscription_created.providing_args)
+
+    def test_subscription_created_call(self):
+        """Subscription created signal must be called with some name in subscriptions:new"""
+        mock = Mock()
+        subscription_created.connect(mock)
+
+        data = dict(name='Paulo Romano', cpf='12345678901',
+                    email='pauloromanocarvalho@gmail.com', phone='11-55556666')
+
+        response = self.client.post(resolve_url('subscriptions:new'), data)
+
+        mock.assert_called_once_with(None, data.get('name'))
