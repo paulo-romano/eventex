591b27eacc15e62755261f24983e1fc4fd6de1bc
Paulo Romano <pauloromanocarvalho@gmail.com>
2016-02-04 21:07:49 -0200

    Subscription created signal must have name param

--- eventex/subscriptions/tests/test_signals.py
+++ eventex/subscriptions/tests/test_signals.py
@@ -7,3 +7,7 @@ class SignalsTest(TestCase):
     def test_subscription_created_signal(self):
         """Must have a subscription created signal"""
         self.assertIsInstance(subscription_created, Signal)
+
+    def test_subscription_created_signal_param(self):
+        """Subscription created signal must have name param"""
+        self.assertIn('name', subscription_created.providing_args)
