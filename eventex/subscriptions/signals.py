2fad25a6b56f926456841695c6d36f6bd98a53c7
Paulo Romano <pauloromanocarvalho@gmail.com>
2016-02-04 22:33:07 -0200

    Receiver implemented

--- eventex/subscriptions/signals.py
+++ eventex/subscriptions/signals.py
@@ -1,4 +1,9 @@
 from django.core.signals import Signal
-
+from django.dispatch import receiver
 
 subscription_created = Signal(providing_args=['name'])
+
+
+@receiver(subscription_created)
+def send_warning_admin(sender, **kwargs):
+    pass
